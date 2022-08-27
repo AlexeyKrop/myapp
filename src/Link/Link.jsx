85e7a6b23e6bb0e9a39bb4bce65246f2bdcba643
EnzoK <kropotin_aleksey@mail.ru>
2022-08-27 12:25:01 +0300

    first commit

---
+++ src/Link/Link.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+
+const STATUS = {
+  HOVERED: 'hovered',
+  NORMAL: 'normal',
+};
+
+export default function Link({page, children}) {
+
+  const [status, setStatus] = useState(STATUS.NORMAL);
+
+  const onMouseEnter = () => {
+    setStatus(STATUS.HOVERED);
+  };
+
+  const onMouseLeave = () => {
+    setStatus(STATUS.NORMAL);
+  };
+
+  return (
+<>
+  <h1>Hello</h1>
+    <a
+      className={status}
+      href={page || '#'}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+    >
+      {children}
+    </a>
+</>
+  );
+
+}
